ca5d5ef0744254f6faaaaef4e120340e569baeeb
ajaygupta12-rgb <ajaygupta12@eng.rizvi.edu.in>
2026-09-10 13:46:22 +0530

    v2.0 - added result management

--- lms.py
+++ lms.py
@@ -1,4 +1,4 @@
-# Learning Management System - Version 1.1
+# Learning Management System - Version 2.0
 
 courses = ["Python", "DBMS", "Computer Networks"]
 
@@ -13,6 +13,10 @@ def enroll_student(student, course):
 def submit_assignment(student, assignment):
     print(student, "submitted", assignment)
 
+def give_result(student, course, marks):
+    print(student, "scored", marks, "in", course)
+
 display_courses()
 enroll_student("Student1", "Python")
 submit_assignment("Student1", "Python Assignment")
+give_result("Student1", "Python", 85)
